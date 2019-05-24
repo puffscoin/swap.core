e797195f928d24bfa971b32ffce4ed25847fb45a
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-24 18:54:26 -0300

    Update erc20.js

--- src/swap.app/util/erc20.js
+++ src/swap.app/util/erc20.js
@@ -4,8 +4,8 @@ import { isCoinAddress, isPublicKey } from './typeforce'
 const register = (code, precision) => {
   constants.COINS[code] = code.toUpperCase()
   constants.COINS_PRECISION[code.toUpperCase()] = precision
-  isCoinAddress[code.toUpperCase()] = isCoinAddress.ETH
-  isPublicKey[code.toUpperCase()] = isPublicKey.ETH
+  isCoinAddress[code.toUpperCase()] = isCoinAddress.PUFFS
+  isPublicKey[code.toUpperCase()] = isPublicKey.PUFFS
 }
 
 export default {
