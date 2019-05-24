8bd82ccc5a6d8e674526e64761c5aa36c7f17182
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-24 18:52:39 -0300

    Update COINS_PRECISION.js

--- src/swap.app/constants/COINS_PRECISION.js
+++ src/swap.app/constants/COINS_PRECISION.js
@@ -1,20 +1,20 @@
-import { ETH_TOKENS } from './COINS'
+import { PUFFS_TOKENS } from './COINS'
 
 export const NATIVE_PRECISION = {
   BTC: 8,
   BCH: 8,
-  ETH: 18,
+  PUFFS: 18,
   LTC: 8,
   USDT: 8,
 }
 
-export const ETH_TOKEN_PRECISION =
-  Object.values(ETH_TOKENS).reduce((acc, token) => ({
+export const PUFFS_TOKEN_PRECISION =
+  Object.values(PUFFS_TOKENS).reduce((acc, token) => ({
     ...acc,
     [token]: 18,
   }), {})
 
 export default {
-  ...ETH_TOKEN_PRECISION,
+  ...PUFFS_TOKEN_PRECISION,
   ...NATIVE_PRECISION,
 }
