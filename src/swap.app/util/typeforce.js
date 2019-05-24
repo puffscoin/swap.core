6d8b53867f5ae0d88ca0461c8564015a57ee2d13
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-24 18:55:25 -0300

    Update typeforce.js

--- src/swap.app/util/typeforce.js
+++ src/swap.app/util/typeforce.js
@@ -1,6 +1,6 @@
 import typeforce from 'typeforce'
 import constants from '../constants'
-import { ETH_TOKENS } from '../constants/COINS'
+import { PUFFS_TOKENS } from '../constants/COINS'
 
 const check = (...args) => {
   try {
@@ -18,7 +18,7 @@ const isCoinName = (value) => Object.values(constants.COINS).map((v) => v.toLowe
 
 const isCoinAddress = {
   [constants.COINS.eos]: (value) => typeof value === 'string' && /^[a-z][a-z1-5.]{0,10}([a-z1-5]|^\.)[a-j1-5]?$/.test(value),
-  [constants.COINS.eth]: (value) => typeof value === 'string' && /^0x[A-Fa-f0-9]{40}$/.test(value),
+  [constants.COINS.puffs]: (value) => typeof value === 'string' && /^0x[A-Fa-f0-9]{40}$/.test(value),
   [constants.COINS.btc]: (value) => typeof value === 'string' && /^[A-Za-z0-9]{26,35}$/.test(value),
   [constants.COINS.bch]: (value) => typeof value === 'string' && /^[A-Za-z0-9:]{26,54}$/.test(value),
   [constants.COINS.ltc]: (value) => typeof value === 'string' && /^[A-Za-z0-9]{34}$/.test(value),
@@ -27,16 +27,16 @@ const isCoinAddress = {
 
 const isPublicKey = {
   [constants.COINS.eos]: '?String',
-  [constants.COINS.eth]: '?String', // TODO we don't have / use eth publicKey
+  [constants.COINS.puffs]: '?String', // TODO we don't have / use eth publicKey
   [constants.COINS.btc]: (value) => typeof value === 'string' && /^[A-Za-z0-9]{66}$/.test(value),
   [constants.COINS.bch]: (value) => typeof value === 'string' && /^[A-Za-z0-9]{66}$/.test(value),
   [constants.COINS.ltc]: (value) => typeof value === 'string' && /^[A-Za-z0-9]{66}$/.test(value),
   [constants.COINS.usdt]: '?String', // TODO we don't have / use nim publicKey
 }
 
-Object.keys(ETH_TOKENS).forEach((tokenCode) => {
-  isCoinAddress[ETH_TOKENS[tokenCode]] = (value) => typeof value === 'string' && /^0x[A-Fa-f0-9]{40}$/.test(value)
-  isPublicKey[ETH_TOKENS[tokenCode]] = '?String'
+Object.keys(PUFFS_TOKENS).forEach((tokenCode) => {
+  isCoinAddress[PUFFS_TOKENS[tokenCode]] = (value) => typeof value === 'string' && /^0x[A-Fa-f0-9]{40}$/.test(value)
+  isPublicKey[PUFFS_TOKENS[tokenCode]] = '?String'
 })
 
 export default {
