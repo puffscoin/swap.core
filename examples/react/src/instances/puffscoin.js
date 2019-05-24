7e9c7e0c70faffcd2744c4a6c826e5c0cf460f49
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-24 14:55:27 -0300

    Update and rename ethereum.js to puffscoin.js

--- examples/react/src/instances/puffscoin.js
+++ examples/react/src/instances/puffscoin.js
@@ -4,7 +4,7 @@ import { request } from '../util'
 
 const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/JCnK5ifEPH9qcQkX0Ahl'))
 
-class Ethereum {
+class Puffscoin {
 
   constructor() {
     this.core = web3
@@ -14,26 +14,26 @@ class Ethereum {
     let account
 
     if (privateKey) {
-      account = this.core.eth.accounts.privateKeyToAccount(privateKey)
+      account = this.core.puffs.accounts.privateKeyToAccount(privateKey)
     }
     else {
-      account = this.core.eth.accounts.create()
-      this.core.eth.accounts.wallet.add(account)
+      account = this.core.puffs.accounts.create()
+      this.core.puffs.accounts.wallet.add(account)
     }
 
-    this.core.eth.accounts.wallet.add(account.privateKey)
+    this.core.puffs.accounts.wallet.add(account.privateKey)
 
-    console.info('Logged in with Ethereum', account)
+    console.info('Logged in with PUFFScoin', account)
 
     return account
   }
 
   fetchBalance(address) {
-    return this.core.eth.getBalance(address)
+    return this.core.puffs.getBalance(address)
       .then((wei) => {
         const balance = Number(this.core.utils.fromWei(wei))
 
-        console.log('ETH balance:', balance)
+        console.log('PUFFS balance:', balance)
 
         return balance
       })
@@ -46,9 +46,9 @@ class Ethereum {
 }
 
 
-export default new Ethereum()
+export default new Puffscoin()
 
 export {
-  Ethereum,
+  Puffscoin,
   web3,
 }
