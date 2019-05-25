1ca49ff09934f9581d2b44224c01f7885ed7d2e6
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-25 12:20:28 -0300

    Update and rename ethSwap.test.js to puffsSwap.test.js

--- tests/puffsSwap.test.js
+++ tests/puffsSwap.test.js
@@ -1,7 +1,7 @@
 import SwapApp, { SwapInterface } from 'swap.app'
 import { mainnet } from 'simple/src/instances/ethereum'
 import bitcoin from 'bitcoinjs-lib'
-import { EthSwap } from 'swap.swaps'
+import { PuffsSwap } from 'swap.swaps'
 import config from './config'
 
 jest.mock('swap.app')
@@ -15,21 +15,21 @@ const secret      = 'c0809ce9f484fdcdfb2d5aabd609768ce0374ee97a1a5618ce4cd3f16c0
 const secretHash  = 'c0933f9be51a284acb6b1a6617a48d795bdeaa80'
 const lockTime    = 1521171580
 
-const ethSwap = new EthSwap({
-  ...config.ethSwap,
+const puffsSwap = new PuffsSwap({
+  ...config.puffsSwap,
   fetchBalance: (address) => 10,
   estimateGasPrice: ({ speed } = {}) => mainnet().estimateGasPrice({ speed }),
 })
 
-xtest('ethSwaps can estimate tx fee', async () => {
-  const result = await ethSwap.estimateGasPrice({ speed: 'normal '})
+xtest('puffsSwaps can estimate tx fee', async () => {
+  const result = await puffsSwap.estimateGasPrice({ speed: 'normal '})
   const expected = 1e9
 
   expect(result).toBeGreaterThanOrEqual(expected)
 })
 
-test('ethSwaps can fetch faked balance', async () => {
-  const result = await ethSwap.fetchBalance('0x60c205722c6c797c725a996cf9cca11291f90749')
+test('puffsSwaps can fetch faked balance', async () => {
+  const result = await puffsSwap.fetchBalance('0x60c205722c6c797c725a996cf9cca11291f90749')
   expect(result).not.toBeNull()
   expect(result).toEqual(10)
 })
